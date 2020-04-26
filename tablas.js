e58b504876e57bf7ae490ed0a9d9a21cf2c2613c
Lara <laraeduardo9601@gmail.com>
2020-04-26 17:22:14 -0500

    Función para calcular las tablas

---
+++ tablas.js
@@ -0,0 +1,18 @@
+var nr;
+nr = prompt('¿Qué tabla quieres visualizar?', '0');
+
+nr = parseInt(nr);
+document.write("TABLA DEL "+nr);
+document.write("<hr>");
+document.write("<table style='margin: 0 auto;'>");
+
+var x = 0;
+
+for (x = 1; x <= 10; x++) {
+  tabel = nr * x;
+  document.write("<tr><td>");
+  document.write(nr + "x" + x + "=" + tabel + "<br>");
+  document.write("</td></tr>");
+}
+
+document.write("</table>");
